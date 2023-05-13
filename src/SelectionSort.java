0950b47d8a8d6f07d867f4302bdc1b5585d4be57
Belkrane <jonghagi77@gmail.com>
2023-05-13 14:40:23 +0900

    Implements selection sort algorithm

---
+++ src/SelectionSort.java
@@ -0,0 +1,22 @@
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] array = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
+
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[minIndex] > array[j]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+
+        for(int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+    }
+}
