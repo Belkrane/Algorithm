1447298f0dcd7f5dd68513e92f29440ef3690a39
Belkrane <jonghagi77@gmail.com>
2023-05-17 15:28:44 +0900

    Complete implement quick sort algorithm

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class QuickSort {
     public static void main(String[] args) {
         int[] array = {5, 7, 9, 0, 3, 1, 6, 2, 4, 8};
@@ -14,31 +16,45 @@ public class QuickSort {
         int leftIndex = 1;
         int rightIndex = array.length -1;
 
+        if(array.length == 1){
+            return array;
+        }
 
+        while(true){
+            
+            if(leftIndex > rightIndex){
 
-        while(leftIndex <= rightIndex){
+                if(array[rightIndex] < array[pivot]){
+                    int temp = array[pivot];
+                    array[pivot] = array[rightIndex];
+                    array[rightIndex] = temp;
+                }
+                break;
+            }
             if(array[leftIndex] > array[pivot] && array[rightIndex] < array[pivot]){
                 int temp = array[leftIndex];
                 array[leftIndex] = array[rightIndex];
                 array[rightIndex] = temp;
             }
 
-            if(array[leftIndex] < array[pivot]){
+            if(array[leftIndex] < array[pivot] && leftIndex < array.length){
                 leftIndex++;
             }
 
-            if(array[rightIndex] > array[pivot]){
-                rightIndex++;
+            if(array[rightIndex] > array[pivot] && rightIndex > 0){
+                rightIndex--;
             }
         }
 
-        if(array.length == 1 || array.length == 2){
+        if(array.length == 2){
             return array;
         }
 
         int[] smallArray = new int[rightIndex];
         int[] bigArray = new int[array.length - rightIndex - 1];
+
         for(int i = 0; i < array.length; i++){
+
             if(i < rightIndex){
                 smallArray[i] = array[i];
             } else if(i > rightIndex){
@@ -46,8 +62,13 @@ public class QuickSort {
             }
         }
 
-        quick_sort(smallArray);
-        quick_sort(bigArray);
+
+        if(smallArray.length > 0 ){
+            quick_sort(smallArray);
+        }
+        if(bigArray.length > 0){
+            quick_sort(bigArray);
+        }
 
         for(int i = 0; i < array.length; i++){
             if(i < rightIndex){
