771e6c158d28bc739b2d8a82904841c40e182722
Belkrane <jonghagi77@gmail.com>
2023-05-24 13:12:16 +0900

    Test Recursive Function using memory

---
+++ src/FibonacciUsingMemory.java
@@ -0,0 +1,29 @@
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class FibonacciUsingMemory {
+    public static void main(String[] args) {
+
+        long[] array = new long[100];
+
+        for(int i = 0; i < 100; i++){
+            array[i] = 0;
+        }
+
+        System.out.println("fibonacciwithmemory(array, 99) = " + fibonacciwithmemory(array, 99));
+    }
+
+    public static long fibonacciwithmemory(long[] array, int n){
+
+        if(n == 1 || n == 2){
+            return 1;
+        } else if(array[n] != 0){
+            return array[n];
+        } else {
+            array[n] = fibonacciwithmemory(array, (n - 1)) + fibonacciwithmemory(array, (n - 2));
+            System.out.println("n + \" \" + array[n] = " + n + " " + array[n]);
+        }
+        
+        return array[n];
+    }
+}
