389147eea1766abd9562a8af66c27cae039ba089
Belkrane <jonghagi77@gmail.com>
2023-05-27 17:55:33 +0900

    Divided BFS Algorithm as function

--- src/BfsUsingQueue.java
+++ src/BfsUsingQueue.java
@@ -4,10 +4,9 @@ import java.util.Queue;
 public class BfsUsingQueue {
     public static void main(String[] args) {
         LinkedList[] linkedLists = new LinkedList[9];
-        boolean[] isVisited = new boolean[9];
+
         for(int i = 0; i < 9; i++){
             linkedLists[i] = new LinkedList<>();
-            isVisited[i] = false;
         }
 
         linkedLists[1].add(2);
@@ -29,6 +28,17 @@ public class BfsUsingQueue {
         linkedLists[8].add(1);
         linkedLists[8].add(7);
 
+
+        BreadthFirstSearch(linkedLists);
+    }
+
+    public static void BreadthFirstSearch(LinkedList[] linkedList){
+        boolean[] isVisited = new boolean[linkedList.length];
+
+        for(int i = 0; i < linkedList.length; i++){
+            isVisited[i] = false;
+        }
+
         Queue queue = new LinkedList<>();
         int currentNode = 1;
         int nextNode = 0;
@@ -40,14 +50,13 @@ public class BfsUsingQueue {
             currentNode = (int)queue.remove();
             System.out.println("currentNode = " + currentNode);
 
-            for(int i = 0; i < linkedLists[currentNode].size(); i++){
-                nextNode = (int)linkedLists[currentNode].get(i);
+            for(int i = 0; i < linkedList[currentNode].size(); i++){
+                nextNode = (int)linkedList[currentNode].get(i);
                 if(!isVisited[nextNode]){
                     queue.add(nextNode);
                     isVisited[nextNode] = true;
                 }
             }
         }
-
     }
 }
